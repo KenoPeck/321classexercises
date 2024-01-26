d1fd5814b809cb2bc067bf5ccd5b6a63f8452420
KenoPeck <cornelius.peck@wsu.edu>
2024-01-26 13:01:17 -0800

    added skeleton code and test

--- HelloWorldTests/GlobalUsings.cs
+++ HelloWorldTests/GlobalUsings.cs
@@ -1,2 +1,3 @@
 global using NUnit.Framework;
 global using HelloWorld.Math;
+global using HelloWorld.Date;
---
+++ In-Class-Exercises/HelloWorld/Date.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace HelloWorld.Date
+{
+    public class Date
+    {
+        public static bool IsLeap(int year)
+        {
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 != 0)
+            //    {
+            //        return true;
+            //    }
+            //    else if (year % 400 == 0)
+            //    {
+            //        return true;
+            //    }
+            //}
+            return false;
+        }
+    }
+}
